b49500f2413d4b4fe685947b7306fab8090b6081
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-24 09:32:37 +0200

    Add plugins in build.gradle.kts

--- 01-encapsulation/build.gradle.kts
+++ 01-encapsulation/build.gradle.kts
@@ -1,1 +1,12 @@
-plugins { java }
+plugins { 
+    java 
+    application
+    checkstyle
+    }
+repositories { mavenCentral() } // Configura Gradle per cercare e scaricare da Maven Central
+dependencies { // Elenco di librerie che ci servono
+  implementation("com.omertron:API-OMDB:1.5")
+}
+application { // Configurazione dell'esecuzione, la main class è sufficiente
+    mainClass.set("it.unibo.encapsulation.TestAllClasses")
+}
