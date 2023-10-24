3212a0178a8a8a8d9760431de42bc8896bdeec33
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-24 10:05:23 +0200

    Fix build.gradle.kts

--- 01-encapsulation/build.gradle.kts
+++ 01-encapsulation/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
     checkstyle
     }
 repositories { mavenCentral() } // Configura Gradle per cercare e scaricare da Maven Central
-dependencies { // Elenco di librerie che ci servono
-  implementation("com.omertron:API-OMDB:1.5")
-}
+
 application { // Configurazione dell'esecuzione, la main class è sufficiente
-    mainClass.set("it.unibo.encapsulation.TestAllClasses")
+    mainClass.set("it.unibo.encapsulation.Main")
 }
