3c96983602d1e77ef1f7e8e805826273fbf0ca59
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-26 15:38:40 +0200

    Fix checkstyle

--- 03-composition/src/main/java/it/unibo/composition/Professor.java
+++ 03-composition/src/main/java/it/unibo/composition/Professor.java
@@ -12,7 +12,13 @@ public class Professor implements User {
     private String password;
     private String[] courses;
 
-    public Professor(int id, String name, String surname, String password, String[] courses) {
+    public Professor(
+        int id, 
+        String name, 
+        String surname, 
+        String password, 
+        String[] courses
+    ) {
         this.id = id;
         this.name = name;
         this.surname = surname;
--- 03-composition/src/main/java/it/unibo/composition/Testing.java
+++ 03-composition/src/main/java/it/unibo/composition/Testing.java
@@ -5,21 +5,64 @@ public class Testing {
     public static void main(final String[] args) {
 
         // 1)Creare 3 studenti a piacere
-        Student andreaRossi = new Student(1, "Andrea","Rossi", "andrearossi1", 2023);
-        Student alexBianchi = new Student(2, "Alex","Bianchi", "alexbianchi2", 2023);
-        Student marcoVerdi = new Student(3, "Marco","Verdi", "marcoverdi3", 2022);
+        Student andreaRossi = new Student(
+                                1, 
+                                "Andrea",
+                                "Rossi", 
+                                "andrearossi1",
+                                2023);
+        Student alexBianchi = new Student(
+                                2, 
+                                "Alex",
+                                "Bianchi", 
+                                "alexbianchi2", 
+                                2023);
+        Student marcoVerdi = new Student(
+                                3, 
+                                "Marco",
+                                "Verdi", 
+                                "marcoverdi3",
+                                2022);
 
         // 2)Creare 2 docenti a piacere
-        Professor angeloFilaseta = new Professor(2,"Angelo","Filaseta","angelofilseta2", new String[]{"PSS"});
-        Professor robertoCasadei = new Professor(1,"Roberto","Casadei", "robertocasadei1", new String[]{"Algoritmi"});
+        Professor angeloFilaseta = new Professor(
+                                    2,
+                                    "Angelo",
+                                    "Filaseta",
+                                    "angelofilseta2",
+                                    new String[]{"PSS"});
+        Professor robertoCasadei = new Professor(
+                                    1,
+                                    "Roberto",
+                                    "Casadei", 
+                                    "robertocasadei1", 
+                                    new String[]{"Algoritmi"});
 
         // 3) Creare due aulee di esame, una con 100 posti una con 80 posti
-        ExamRoom room1 = new ExamRoom(100, "A1", true, false);
-        ExamRoom room2 = new ExamRoom(80, "A2", true, true);
+        ExamRoom room1 = new ExamRoom(
+                            100,
+                            "A1",
+                            true, 
+                            false);
+        ExamRoom room2 = new ExamRoom(
+                            80, 
+                            "A2", 
+                            true, 
+                            true);
 
         // 4) Creare due esami, uno con nMaxStudents=10, l'altro con nMaxStudents=2
-        Exam exam1 = new Exam(1, 10, "PSS", angeloFilaseta, room1);
-        Exam exam2 = new Exam(2, 2, "Algoritmi", robertoCasadei, room2);
+        Exam exam1 = new Exam(
+                        1, 
+                        10, 
+                        "PSS", 
+                        angeloFilaseta, 
+                        room1);
+        Exam exam2 = new Exam(
+                        2, 
+                        2, 
+                        "Algoritmi", 
+                        robertoCasadei, 
+                        room2);
 
         // 5) Iscrivere tutti e 3 gli studenti agli esami
         exam1.registerStudent(andreaRossi);
